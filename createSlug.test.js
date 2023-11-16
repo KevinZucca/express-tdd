95e00a4b0a7701b7c819190af3c51281a9b01d52
KevinZucca <kev.94@live.it>
2023-11-16 16:38:18 +0100

    feat: error tests passed

--- createSlug.test.js
+++ createSlug.test.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { test, expect } = require("@jest/globals");
 const createSlug = require("./utilities/createSlug");
-const list = require("./posts.json");
 
 test("La funzione dovrebbe restituire una stringa", () => {
   const title = "Gaming in Realtà Virtuale: Il Futuro del Divertimento";
+  const list = require("./posts.json");
   const result = createSlug(title, list);
 
   expect(typeof result).toBe("string");
@@ -35,4 +35,19 @@ test("La funzione dovrebbe incrementare di 1 lo slug quando esiste già", () => 
   expect(result).toBe(
     "gaming-in-realtà-virtuale:-il-futuro-del-divertimento-1"
   );
+});
+
+test("La funzione dovrebbe lanciare un errore in caso di titolo mancante o formato errato", () => {
+  const title = 36458645;
+  const result = () => createSlug(title, list);
+
+  expect(result).toThrow("Il titolo non è presente o in formato errato");
+});
+
+test("La funzione dovrebbe lanciare un errore in caso mancato array dei post", () => {
+  const title = "Gaming in Realtà Virtuale: Il Futuro del Divertimento";
+  let postList;
+  const result = () => createSlug(title, postList);
+
+  expect(result).toThrow("La lista non è presente");
 });
